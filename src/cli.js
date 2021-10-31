a62404e745c651452685ac827088219d362dc7e1
MaximKalinchuk <kalinchuk.maxim@mail.ru>
2021-10-31 11:54:15 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const name = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+const name = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
+export default name;
